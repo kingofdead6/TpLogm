98e801bf468e16acc100846fc5af7b1f08ab7496
nejem Eddine Mekentichi <83135662+Nejmeddine-Mek@users.noreply.github.com>
2025-05-24 20:00:57 +0100

    logic is functional now, we will add interface and user realted functionalities soon

--- src/main/main.c
+++ src/main/main.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include "../fileHandlers/fileHandlers.h"
 #include "../stringHandlers/stringHandlers.h"
+//#include "../interface/interface.h"
 #include <string.h>
-
+#include <windows.h>
 int main(){
     /*int count = getClauseCount("A|B&B|C|D&!A|!D&P|!R|Q&Q|S");
     //printf("clauses count %d", getClauseCount("A|B&B|C"));
@@ -12,6 +13,9 @@ int main(){
     for(int i = 0; i < count ; i++){
         printf("%s\n", output[i]);
     } */
-    testClauses("A|C&!A|!C");
+    testClauses("A|B&!A|C&!B|C&!C|D&!D");
+    //MessageBeep(MB_ICONASTERISK);
+    //MessageBox(NULL,"Do you want to continue", "Question", MB_YESNO | MB_ICONQUESTION);
+   // WinMain(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
     return 0;
 }
